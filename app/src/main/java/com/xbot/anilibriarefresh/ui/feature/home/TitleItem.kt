a862c6c695f37bce8183a966a03eadcc9eb5b024
Niks0nP <nikita.niko40@gmail.com>
2024-10-09 23:17:43 +0300

    add title button

--- app/src/main/java/com/xbot/anilibriarefresh/ui/feature/home/TitleItem.kt
+++ app/src/main/java/com/xbot/anilibriarefresh/ui/feature/home/TitleItem.kt
@@ -1,8 +1,25 @@
 package com.xbot.anilibriarefresh.ui.feature.home
 
 import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.xbot.domain.model.TitleModel
 
 @Composable
@@ -36,12 +53,50 @@ private fun TitleItemContent(
     title: TitleModel,
     onClick: (Int) -> Unit
 ) {
-    //TODO: Layout для загруженного Title
+    Row(
+        modifier
+            .fillMaxWidth()
+            .height(170.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(Color.White)
+            .padding(16.dp)) {
+        Box(
+            modifier
+                .size(height = 140.dp, width = 100.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .background(Color.LightGray)) {
+        }
+        Column(modifier.padding(start = 16.dp).fillMaxHeight()) {
+            Text(
+                text = title.name,
+                fontWeight = FontWeight.Bold
+            )
+            Text(text = title.description,
+                modifier.padding(top = 5.dp)
+                    .weight(1f),
+                maxLines = 3,
+                overflow = TextOverflow.Ellipsis)
+        }
+    }
 }
 
 @Composable
 private fun LoadingTitleItem(
     modifier: Modifier = Modifier
 ) {
-    //TODO: Layout для процесса загрузки
+    Box(modifier.fillMaxWidth().height(120.dp))
+}
+
+@Preview
+@Composable
+private fun PreviewTitleItem() {
+    val titleModel = TitleModel(
+        id = 1,
+        name = "Атака титанов",
+        description = "Аниме об уничтожении мира, где главный герой может уничтожить весь мир и не хочет чтобы его друзья погибали",
+        posterUrl = null,
+        uploadedTime = 0L
+    )
+    TitleItem(title = titleModel) { }
+
 }
