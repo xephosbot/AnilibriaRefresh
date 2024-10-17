a5c97f8d8722e2dde8a8698bf106cbe1e44cf34a
xephosbot <xephosbot@gmail.com>
2024-10-17 18:36:59 +0300

    Splash screen added

--- app/src/main/java/com/xbot/anilibriarefresh/ui/MainActivity.kt
+++ app/src/main/java/com/xbot/anilibriarefresh/ui/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.xbot.anilibriarefresh.ui
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.splashscreen.SplashScreenViewProvider
+import androidx.interpolator.view.animation.FastOutLinearInInterpolator
 import com.xbot.anilibriarefresh.ui.theme.AnilibriaTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        splashScreen.setOnExitAnimationListener { splashScreenViewProvider ->
+            onSplashScreenExit(splashScreenViewProvider)
+        }
+
         enableEdgeToEdge()
 
         setContent {
@@ -18,5 +31,26 @@ class MainActivity : ComponentActivity() {
                 AnilibriaApp()
             }
         }
+    }
+
+    private fun onSplashScreenExit(splashScreenViewProvider: SplashScreenViewProvider) {
+        val accelerateInterpolator = FastOutLinearInInterpolator()
+        val splashScreenView = splashScreenViewProvider.view
+        val iconView = splashScreenViewProvider.iconView
+
+        val animatorSet = AnimatorSet()
+        animatorSet.playTogether(
+            ObjectAnimator.ofFloat(splashScreenView, View.ALPHA, 1f, 0f),
+            ObjectAnimator.ofFloat(iconView, View.ALPHA, 1f, 0f)
+        )
+        animatorSet.duration = SPLASHSCREEN_ALPHA_ANIMATION_DURATION
+        animatorSet.interpolator = accelerateInterpolator
+
+        animatorSet.doOnEnd { splashScreenViewProvider.remove() }
+        animatorSet.start()
+    }
+
+    companion object {
+        private const val SPLASHSCREEN_ALPHA_ANIMATION_DURATION = 200L
     }
 }
