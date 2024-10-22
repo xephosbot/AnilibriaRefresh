6696c0cc2071405de7ed3c15019e7807b2aa4f6d
xephosbot <xephosbot@gmail.com>
2024-10-22 21:12:33 +0300

    Add serializing annotations to PublishStatusEnum

--- anilibria-api/src/main/java/com/xbot/api/models/enums/PublishStatusEnum.kt
+++ anilibria-api/src/main/java/com/xbot/api/models/enums/PublishStatusEnum.kt
@@ -1,8 +1,12 @@
 package com.xbot.api.models.enums
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 enum class PublishStatusEnum(private val value: String) {
-    IS_ONGOING("IS_ONGOING"),
-    IS_NOT_ONGOING("IS_NOT_ONGOING");
+    @SerialName("IS_ONGOING") IS_ONGOING("IS_ONGOING"),
+    @SerialName("IS_NOT_ONGOING") IS_NOT_ONGOING("IS_NOT_ONGOING");
 
     override fun toString(): String = value
 }
