580ec4638e0466fda5202e1c01bcec3f652f6c05
xephosbot <xephosbot@gmail.com>
2024-11-06 19:21:40 +0300

    PosterImage component imporments

--- app/src/main/java/com/xbot/anilibriarefresh/ui/components/PosterImage.kt
+++ app/src/main/java/com/xbot/anilibriarefresh/ui/components/PosterImage.kt
@@ -1,9 +1,15 @@
 package com.xbot.anilibriarefresh.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import coil3.compose.AsyncImage
+import coil3.compose.SubcomposeAsyncImage
+import com.xbot.anilibriarefresh.ui.utils.LocalShimmer
+import com.xbot.anilibriarefresh.ui.utils.shimmerSafe
 import com.xbot.domain.model.PosterModel
 
 @Composable
@@ -12,10 +18,20 @@ fun PosterImage(
     poster: PosterModel,
     contentDescription: String? = null
 ) {
-    AsyncImage(
+    val shimmer = LocalShimmer.current
+
+    SubcomposeAsyncImage (
         modifier = modifier,
         model = poster,
         contentDescription = contentDescription,
-        contentScale = ContentScale.Crop
+        contentScale = ContentScale.Crop,
+        loading = {
+            Box(
+                modifier = modifier
+                    .fillMaxSize()
+                    .shimmerSafe(shimmer)
+                    .background(Color.LightGray)
+            )
+        }
     )
 }
