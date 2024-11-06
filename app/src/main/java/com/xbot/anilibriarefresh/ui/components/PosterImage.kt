16bf7a2197ccdac6cf6b0e7d23e9de84c0d7d0f3
xephosbot <xephosbot@gmail.com>
2024-11-06 21:53:20 +0300

    PosterImage updated

--- app/src/main/java/com/xbot/anilibriarefresh/ui/components/PosterImage.kt
+++ app/src/main/java/com/xbot/anilibriarefresh/ui/components/PosterImage.kt
@@ -1,13 +1,20 @@
 package com.xbot.anilibriarefresh.ui.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import coil3.compose.SubcomposeAsyncImage
+import coil3.compose.AsyncImage
 import com.xbot.anilibriarefresh.ui.utils.LocalShimmer
 import com.xbot.anilibriarefresh.ui.utils.shimmerSafe
 import com.xbot.domain.model.PosterModel
@@ -16,22 +23,35 @@ import com.xbot.domain.model.PosterModel
 fun PosterImage(
     modifier: Modifier = Modifier,
     poster: PosterModel,
-    contentDescription: String? = null
+    contentDescription: String? = null,
+    contentScale: ContentScale = ContentScale.Crop
 ) {
+    var isLoading by rememberSaveable(poster) { mutableStateOf(true) }
     val shimmer = LocalShimmer.current
 
-    SubcomposeAsyncImage (
-        modifier = modifier,
-        model = poster,
-        contentDescription = contentDescription,
-        contentScale = ContentScale.Crop,
-        loading = {
+    Box(
+        modifier = modifier
+    ) {
+        AsyncImage(
+            modifier = Modifier.fillMaxSize(),
+            model = poster,
+            contentDescription = contentDescription,
+            contentScale = contentScale,
+            onSuccess = {
+                isLoading = false
+            }
+        )
+        AnimatedVisibility(
+            visible = isLoading,
+            enter = fadeIn(),
+            exit = fadeOut()
+        ) {
             Box(
-                modifier = modifier
+                modifier = Modifier
                     .fillMaxSize()
                     .shimmerSafe(shimmer)
                     .background(Color.LightGray)
             )
         }
-    )
+    }
 }
