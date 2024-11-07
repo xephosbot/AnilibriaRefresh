9c5eb9462dd9ea0a4f782542971576f614e7582a
xephosbot <xephosbot@gmail.com>
2024-11-08 00:41:50 +0300

    Some fixes

--- data/src/main/java/com/xbot/data/di/DataSourceModule.kt
+++ data/src/main/java/com/xbot/data/di/DataSourceModule.kt
@@ -1,6 +1,7 @@
 package com.xbot.data.di
 
 import com.xbot.api.service.AnilibriaClient
+import com.xbot.data.datasource.FilterDataSource
 import com.xbot.data.datasource.TitleDataSource
 import dagger.Module
 import dagger.Provides
@@ -15,5 +16,11 @@ object DataSourceModule {
     @Singleton
     fun provideTitleDataSource(client: AnilibriaClient): TitleDataSource {
         return TitleDataSource(client)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFilterDataSource(client: AnilibriaClient): FilterDataSource {
+        return FilterDataSource(client)
     }
 }
--- data/src/main/java/com/xbot/data/di/RepositoryModule.kt
+++ data/src/main/java/com/xbot/data/di/RepositoryModule.kt
@@ -1,7 +1,10 @@
 package com.xbot.data.di
 
+import com.xbot.data.datasource.FilterDataSource
 import com.xbot.data.datasource.TitleDataSource
+import com.xbot.data.repository.FiltersRepositoryImpl
 import com.xbot.data.repository.TitleRepositoryImpl
+import com.xbot.domain.repository.FiltersRepository
 import com.xbot.domain.repository.TitleRepository
 import dagger.Module
 import dagger.Provides
@@ -16,5 +19,11 @@ object RepositoryModule {
     @Singleton
     fun provideTitleRepository(dataSource: TitleDataSource): TitleRepository {
         return TitleRepositoryImpl(dataSource)
+    }
+
+    @Provides
+    @Singleton
+    fun provideFiltersRepository(dataSource: FilterDataSource): FiltersRepository {
+        return FiltersRepositoryImpl(dataSource)
     }
 }
