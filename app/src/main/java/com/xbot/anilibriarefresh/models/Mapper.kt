9f02ee6bc9ba1f8bf5bfaa7d292d91dcf2304b01
Niks0nP <nikita.niko40@gmail.com>
2024-11-10 01:09:43 +0300

    add mappers enum to stringResource

--- app/src/main/java/com/xbot/anilibriarefresh/models/Mapper.kt
+++ app/src/main/java/com/xbot/anilibriarefresh/models/Mapper.kt
@@ -7,7 +7,12 @@ import com.xbot.anilibriarefresh.ui.utils.StringResource
 import com.xbot.domain.models.PosterModel
 import com.xbot.domain.models.TitleDetailModel
 import com.xbot.domain.models.TitleModel
+import com.xbot.domain.models.enums.AgeRating
+import com.xbot.domain.models.enums.ProductionStatus
+import com.xbot.domain.models.enums.PublishStatus
 import com.xbot.domain.models.enums.ReleaseType
+import com.xbot.domain.models.enums.Season
+import com.xbot.domain.models.enums.SortingTypes
 
 fun TitleModel.toTitleUi(): Title {
     return Title(
@@ -60,6 +65,55 @@ private fun ReleaseType.toStringResource(): StringResource {
         ReleaseType.MOVIE -> R.string.release_type_movie
         ReleaseType.DORAMA -> R.string.release_type_dorama
         ReleaseType.SPECIAL -> R.string.release_type_special
+    }
+    return StringResource.Text(resId)
+}
+
+private fun AgeRating.toStringResource(): StringResource {
+    val resId = when(this) {
+        AgeRating.R0_PLUS -> R.string.age_rating_0_plus
+        AgeRating.R6_PLUS -> R.string.age_rating_6_plus
+        AgeRating.R12_PLUS -> R.string.age_rating_12_plus
+        AgeRating.R16_PLUS -> R.string.age_rating_16_plus
+        AgeRating.R18_PLUS -> R.string.age_rating_18_plus
+    }
+    return StringResource.Text(resId)
+}
+
+private fun ProductionStatus.toStringResource(): StringResource {
+    val resId = when(this) {
+        ProductionStatus.IS_IN_PRODUCTION -> R.string.production_status_is_in_production
+        ProductionStatus.IS_NOT_IN_PRODUCTION -> R.string.production_status_is_not_in_production
+    }
+    return StringResource.Text(resId)
+}
+
+private fun PublishStatus.toStringResourse(): StringResource {
+    val resId = when(this) {
+        PublishStatus.IS_ONGOING -> R.string.publish_status_is_ongoing
+        PublishStatus.IS_NOT_ONGOING -> R.string.publish_status_is_not_ongoing
+    }
+    return StringResource.Text(resId)
+}
+
+private fun Season.toStringResource(): StringResource {
+    val resId = when(this) {
+        Season.WINTER -> R.string.season_winter
+        Season.SPRING -> R.string.season_spring
+        Season.SUMMER -> R.string.season_summer
+        Season.AUTUMN -> R.string.season_autumn
+    }
+    return StringResource.Text(resId)
+}
+
+private fun SortingTypes.toStringResource(): StringResource {
+    val resId = when(this) {
+        SortingTypes.FRESH_AT_DESC -> R.string.sorting_types_fresh_at_desc
+        SortingTypes.FRESH_AT_ASC -> R.string.sorting_types_fresh_at_asc
+        SortingTypes.RATING_DESC -> R.string.sorting_types_rating_desc
+        SortingTypes.RATING_ASC -> R.string.sorting_types_rating_asc
+        SortingTypes.YEAR_DESC -> R.string.sorting_types_year_desc
+        SortingTypes.YEAR_ASC -> R.string.sorting_types_year_asc
     }
     return StringResource.Text(resId)
 }
