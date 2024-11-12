cfe40b8d7fbee87e2a9e37fb8f0fd4ed9d7210d9
Javokhir Isaev <xephosbot@gmail.com>
2024-11-12 18:52:16 +0300

    BlurUtils: API 31 blur variant implemented

--- app/src/main/java/com/xbot/anilibriarefresh/ui/utils/BlurredBitmap.kt
+++ app/src/main/java/com/xbot/anilibriarefresh/ui/utils/BlurredBitmap.kt
@@ -2,34 +2,49 @@ package com.xbot.anilibriarefresh.ui.utils
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.HardwareRenderer
+import android.graphics.PixelFormat
+import android.graphics.RenderEffect
+import android.graphics.RenderNode
+import android.graphics.Shader
+import android.hardware.HardwareBuffer
+import android.media.ImageReader
+import android.os.Build
 import android.renderscript.Allocation
 import android.renderscript.Element
 import android.renderscript.RenderScript
 import android.renderscript.ScriptIntrinsicBlur
+import androidx.annotation.FloatRange
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 
 @Composable
-fun rememberBlurredBitmap(bitmap: Bitmap, radius: Float): Bitmap {
+fun rememberBlurredBitmap(
+    bitmap: Bitmap,
+    @FloatRange(from = 0.0, to = 25.0) radius: Float
+): Bitmap {
     val context = LocalContext.current
     return remember(context, bitmap, radius) {
         val tempBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
-        BlurUtils.blur(context, tempBitmap, radius)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            BlurUtils.blur(tempBitmap, radius)
+        } else {
+            BlurUtils.blur(context, tempBitmap, radius)
+        }
     }
 }
 
-// TODO: Since RenderScript is deprecated for API 31 and above, a second implementation needs to be added.
-//  https://developer.android.com/guide/topics/renderscript/migrate#migrate-from-blur
-@Suppress("DEPRECATION")
-private object BlurUtils {
+object BlurUtils {
     private const val BITMAP_SCALE = 0.4f
 
-    fun blur(context: Context, image: Bitmap, radius: Float): Bitmap {
-        val width = Math.round(image.width * BITMAP_SCALE)
-        val height = Math.round(image.height * BITMAP_SCALE)
+    @Suppress("DEPRECATION")
+    fun blur(context: Context, bitmap: Bitmap, radius: Float): Bitmap {
+        val width = Math.round(bitmap.width * BITMAP_SCALE)
+        val height = Math.round(bitmap.height * BITMAP_SCALE)
 
-        val inputBitmap = Bitmap.createScaledBitmap(image, width, height, false)
+        val inputBitmap = Bitmap.createScaledBitmap(bitmap, width, height, false)
         val outputBitmap = Bitmap.createBitmap(inputBitmap)
 
         val rs = RenderScript.create(context)
@@ -40,6 +55,46 @@ private object BlurUtils {
         theIntrinsic.setInput(tmpIn)
         theIntrinsic.forEach(tmpOut)
         tmpOut.copyTo(outputBitmap)
+
+        return outputBitmap
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    fun blur(bitmap: Bitmap, radius: Float): Bitmap {
+        val imageReader = ImageReader.newInstance(
+            bitmap.width, bitmap.height,
+            PixelFormat.RGBA_8888, 1,
+            HardwareBuffer.USAGE_GPU_SAMPLED_IMAGE or HardwareBuffer.USAGE_GPU_COLOR_OUTPUT,
+        )
+        val renderNode = RenderNode("BlurEffect")
+        val hardwareRenderer = HardwareRenderer()
+
+        hardwareRenderer.setSurface(imageReader.surface)
+        hardwareRenderer.setContentRoot(renderNode)
+        renderNode.setPosition(0, 0, imageReader.width, imageReader.height)
+        val blurRenderEffect = RenderEffect.createBlurEffect(
+            radius, radius,
+            Shader.TileMode.MIRROR,
+        )
+        renderNode.setRenderEffect(blurRenderEffect)
+
+        val renderCanvas = renderNode.beginRecording()
+        renderCanvas.drawBitmap(bitmap, 0f, 0f, null)
+        renderNode.endRecording()
+        hardwareRenderer.createRenderRequest()
+            .setWaitForPresent(true)
+            .syncAndDraw()
+
+        val image = imageReader.acquireNextImage() ?: throw RuntimeException("No Image")
+        val hardwareBuffer = image.hardwareBuffer ?: throw RuntimeException("No HardwareBuffer")
+        val outputBitmap =  Bitmap.wrapHardwareBuffer(hardwareBuffer, null)
+            ?: throw RuntimeException("Create Bitmap Failed")
+
+        hardwareBuffer.close()
+        image.close()
+        imageReader.close()
+        renderNode.discardDisplayList()
+        hardwareRenderer.destroy()
 
         return outputBitmap
     }
