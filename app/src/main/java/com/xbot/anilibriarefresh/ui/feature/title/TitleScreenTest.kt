0cdf6b748ade92563dad75ef566d947156bc02ab
Javokhir Isaev <xephosbot@gmail.com>
2024-11-12 16:58:22 +0300

    PosterWithBackground composable for TitleScreenTest

--- app/src/main/java/com/xbot/anilibriarefresh/ui/feature/title/TitleScreenTest.kt
+++ app/src/main/java/com/xbot/anilibriarefresh/ui/feature/title/TitleScreenTest.kt
@@ -1,6 +1,9 @@
 package com.xbot.anilibriarefresh.ui.feature.title
 
+import androidx.compose.animation.Crossfade
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -33,15 +36,24 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.skydoves.cloudy.cloudy
+import coil3.compose.AsyncImagePainter
+import coil3.compose.rememberAsyncImagePainter
+import coil3.request.ImageRequest
+import coil3.size.Size
+import coil3.toBitmap
+import com.valentinilk.shimmer.ShimmerBounds
+import com.valentinilk.shimmer.rememberShimmer
+import com.xbot.anilibriarefresh.models.Poster
 import com.xbot.anilibriarefresh.models.TitleDetail
 import com.xbot.anilibriarefresh.models.toPosterUi
 import com.xbot.anilibriarefresh.ui.components.OverlayHeaderLayout
@@ -49,8 +61,12 @@ import com.xbot.anilibriarefresh.ui.components.PosterImage
 import com.xbot.anilibriarefresh.ui.components.rememberOverlayHeaderLayoutScrollBehavior
 import com.xbot.anilibriarefresh.ui.icons.AnilibriaIcons
 import com.xbot.anilibriarefresh.ui.icons.Heart
+import com.xbot.anilibriarefresh.ui.utils.ProvideShimmer
 import com.xbot.anilibriarefresh.ui.utils.fadedEdge
 import com.xbot.anilibriarefresh.ui.utils.only
+import com.xbot.anilibriarefresh.ui.utils.rememberBlurredBitmap
+import com.xbot.anilibriarefresh.ui.utils.shimmerSafe
+import com.xbot.anilibriarefresh.ui.utils.shimmerUpdater
 import com.xbot.domain.models.EpisodeModel
 import org.koin.androidx.compose.koinViewModel
 
@@ -82,101 +98,150 @@ private fun TitleScreenContent(
     OverlayHeaderLayout(
         modifier = modifier,
         headlineContent = {
-            Box(
+            PosterWithBackground(
                 modifier = Modifier
                     .padding(paddingValues.only(WindowInsetsSides.Top))
-                    .fillMaxWidth()
-                    .height(300.dp)
                     .graphicsLayer {
                         alpha = scrollBehavior.state.collapsedFraction
                     },
-                contentAlignment = Alignment.Center,
-            ) {
-                PosterImage(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .fadedEdge(edgeHeight = 200.dp)
-                        .fadedEdge(edgeHeight = 200.dp, bottomEdge = false)
-                        //TODO: Find another way to blur bitmap with supporting SDK >= 24
-                        .cloudy(radius = 500),
-                    poster = title.poster,
-                )
-                PosterImage(
-                    modifier = Modifier
-                        .fillMaxHeight()
-                        .aspectRatio(7f / 10f)
-                        .clip(RoundedCornerShape(16.dp))
-                        .shadow(elevation = 3.dp),
-                    poster = title.poster,
-                    contentScale = ContentScale.Fit,
-                )
-            }
+                poster = title.poster,
+            )
         },
         scrollBehavior = scrollBehavior,
     ) {
-        Surface {
-            LazyColumn(
-                contentPadding = paddingValues.only(WindowInsetsSides.Bottom),
-            ) {
-                item {
-                    Column(
-                        modifier = Modifier.padding(horizontal = 16.dp),
-                    ) {
-                        Spacer(Modifier.height(16.dp))
-                        Text(
-                            text = title.name,
-                            fontSize = 22.sp,
-                            fontWeight = FontWeight.SemiBold,
-                        )
-                        Spacer(Modifier.height(16.dp))
-                        Row {
-                            Button(
-                                modifier = Modifier.weight(1f),
-                                onClick = {},
-                            ) {
-                                Icon(
-                                    imageVector = Icons.Default.PlayArrow,
-                                    contentDescription = null,
-                                )
-                                Spacer(Modifier.width(8.dp))
-                                Text(text = "Смотреть")
-                            }
-                            Spacer(Modifier.width(16.dp))
-                            OutlinedButton(
-                                onClick = {},
-                            ) {
-                                Icon(
-                                    imageVector = AnilibriaIcons.Filled.Heart,
-                                    contentDescription = null,
-                                )
-                                Spacer(Modifier.width(8.dp))
-                                Text(text = "2.1k")
-                            }
-                        }
-                        var expanded by remember { mutableStateOf(false) }
+        Surface(modifier = Modifier.fillMaxSize()) {
+            val shimmer = rememberShimmer(ShimmerBounds.Custom)
 
-                        Box(
-                            modifier = Modifier
-                                .clickable {
-                                    expanded = !expanded
-                                }
-                                .padding(16.dp),
+            ProvideShimmer(shimmer) {
+                LazyColumn(
+                    modifier = Modifier.shimmerUpdater(shimmer),
+                    contentPadding = paddingValues.only(WindowInsetsSides.Bottom),
+                ) {
+                    item {
+                        Column(
+                            modifier = Modifier.padding(horizontal = 16.dp),
                         ) {
+                            Spacer(Modifier.height(16.dp))
                             Text(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .animateContentSize(),
-                                text = title.description,
-                                maxLines = if (expanded) Int.MAX_VALUE else 3,
-                                overflow = TextOverflow.Ellipsis,
+                                text = title.name,
+                                fontSize = 22.sp,
+                                fontWeight = FontWeight.SemiBold,
                             )
+                            Spacer(Modifier.height(16.dp))
+                            Row {
+                                Button(
+                                    modifier = Modifier.weight(3f),
+                                    onClick = {},
+                                ) {
+                                    Icon(
+                                        imageVector = Icons.Default.PlayArrow,
+                                        contentDescription = null,
+                                    )
+                                    Spacer(Modifier.width(8.dp))
+                                    Text(text = "Смотреть")
+                                }
+                                Spacer(Modifier.width(16.dp))
+                                OutlinedButton(
+                                    modifier = Modifier.weight(1.5f),
+                                    onClick = {},
+                                ) {
+                                    Icon(
+                                        imageVector = AnilibriaIcons.Filled.Heart,
+                                        contentDescription = null,
+                                    )
+                                    Spacer(Modifier.width(8.dp))
+                                    Text(text = "2.1k")
+                                }
+                            }
+                            var expanded by remember { mutableStateOf(false) }
+
+                            Box(
+                                modifier = Modifier
+                                    .clickable {
+                                        expanded = !expanded
+                                    }
+                                    .padding(16.dp),
+                            ) {
+                                Text(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .animateContentSize(),
+                                    text = title.description,
+                                    maxLines = if (expanded) Int.MAX_VALUE else 3,
+                                    overflow = TextOverflow.Ellipsis,
+                                )
+                            }
                         }
                     }
+                    items(title.episodes) { episode ->
+                        EpisodeItem(
+                            episode = episode,
+                            onClick = {},
+                        )
+                    }
                 }
-                items(title.episodes) { episode ->
-                    EpisodeItem(
-                        episode = episode,
-                        onClick = {},
+            }
+        }
+    }
+}
+
+@Composable
+private fun PosterWithBackground(
+    modifier: Modifier = Modifier,
+    poster: Poster,
+) {
+    val posterPainter = rememberAsyncImagePainter(
+        model = ImageRequest.Builder(LocalContext.current)
+            .data(poster)
+            .size(Size.ORIGINAL)
+            .build(),
+    )
+
+    val posterImageLoadedState by posterPainter.state.collectAsStateWithLifecycle()
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(350.dp),
+        contentAlignment = Alignment.Center,
+    ) {
+        Crossfade(targetState = posterImageLoadedState) { state ->
+            when (state) {
+                is AsyncImagePainter.State.Success -> {
+                    val posterBitmap = state.result.image.toBitmap()
+                    val blurredPosterBitmap = rememberBlurredBitmap(posterBitmap, radius = 25f)
+
+                    Image(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .fadedEdge(edgeHeight = 200.dp)
+                            .fadedEdge(edgeHeight = 200.dp, bottomEdge = false),
+                        bitmap = blurredPosterBitmap.asImageBitmap(),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                    )
+
+                    Image(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .aspectRatio(7f / 10f)
+                            .clip(RoundedCornerShape(16.dp))
+                            .align(Alignment.Center),
+                        bitmap = posterBitmap.asImageBitmap(),
+                        contentDescription = null,
+                        contentScale = ContentScale.Fit,
+                    )
+                }
+                else -> {
+                    val shimmer = rememberShimmer(ShimmerBounds.View)
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .shimmerSafe(shimmer)
+                            .fadedEdge(edgeHeight = 150.dp)
+                            .fadedEdge(edgeHeight = 150.dp, bottomEdge = false)
+                            .background(Color.LightGray),
                     )
                 }
             }
