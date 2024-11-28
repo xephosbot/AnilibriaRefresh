fd1ef0b75f0e6ba752f20414ed933175bc1dfa24
Javokhir Isaev <xephosbot@gmail.com>
2024-11-28 17:15:09 +0300

    TopAppBar: component cleaned up

--- app/src/main/java/com/xbot/anilibriarefresh/ui/components/TopAppBar.kt
+++ app/src/main/java/com/xbot/anilibriarefresh/ui/components/TopAppBar.kt
@@ -1,75 +1,41 @@
 package com.xbot.anilibriarefresh.ui.components
 
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
-import com.xbot.anilibriarefresh.R
-import com.xbot.anilibriarefresh.navigation.isTopLevelDestination
+import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AnilibriaTopAppBar(
     modifier: Modifier = Modifier,
-    navController: NavHostController,
-    onNavigationClick: () -> Unit = {},
+    title: String,
+    navigationIcon: @Composable () -> Unit = {},
+    actions: @Composable RowScope.() -> Unit = {},
     scrollBehavior: TopAppBarScrollBehavior? = null,
 ) {
-    val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val titleArg = navBackStackEntry?.arguments?.getString("titleName")
-    val isTopLevel = navController.isTopLevelDestination()
-
     TopAppBar(
         modifier = modifier,
-        navigationIcon = {
-            IconButton(
-                onClick = {
-                    when (isTopLevel) {
-                        true -> onNavigationClick()
-                        else -> navController.navigateUp()
-                    }
-                },
-            ) {
-                when (isTopLevel) {
-                    true -> Icon(
-                        imageVector = ImageVector.vectorResource(R.drawable.ic_anilibria),
-                        contentDescription = null,
-                        tint = MaterialTheme.colorScheme.primary,
-                    )
-                    else -> Icon(
-                        imageVector = Icons.AutoMirrored.Default.ArrowBack,
-                        contentDescription = null,
-                    )
-                }
-            }
-        },
+        navigationIcon = navigationIcon,
         title = {
             Text(
-                text = when (isTopLevel) {
-                    true -> stringResource(R.string.app_name)
-                    else -> titleArg ?: "Unknown"
-                },
+                text = title,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold,
                 overflow = TextOverflow.Ellipsis,
                 maxLines = 1,
             )
         },
+        actions = actions,
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = Color.Transparent,
             scrolledContainerColor = Color.Transparent,
