6e7babe803bdb813059673c07a52e52cf04e9eec
Javokhir Isaev <xephosbot@gmail.com>
2024-11-28 17:11:35 +0300

    TestScreen: deleted

--- app/src/main/java/com/xbot/anilibriarefresh/ui/feature/TestScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.xbot.anilibriarefresh.ui.feature
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-
-@Composable
-fun TestScreen(modifier: Modifier = Modifier) {
-
-}
