7ca2651feee23c387b24f1466bc6a11301815521
Javokhir Isaev <xephosbot@gmail.com>
2024-11-28 17:13:10 +0300

    Header: component layout updated

--- app/src/main/java/com/xbot/anilibriarefresh/ui/components/Header.kt
+++ app/src/main/java/com/xbot/anilibriarefresh/ui/components/Header.kt
@@ -1,66 +1,46 @@
 package com.xbot.anilibriarefresh.ui.components
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.xbot.anilibriarefresh.R
-import com.xbot.anilibriarefresh.ui.utils.only
 
 @Composable
 fun Header(
     modifier: Modifier = Modifier,
     title: String,
-    contentPadding: PaddingValues = HeaderDefaults.ContentPadding,
     onClick: (() -> Unit)? = null,
 ) {
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .then(
-                if (onClick != null) {
-                    Modifier.clickable(onClick = onClick)
-                } else {
-                    Modifier
-                },
-            )
-            .padding(contentPadding),
+            .height(48.dp)
+            .padding(horizontal = 16.dp),
         verticalAlignment = Alignment.CenterVertically,
     ) {
         // TODO: Use MaterialTheme.typography style
         Text(
             text = title,
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp,
+            lineHeight = 28.sp,
+            fontWeight = FontWeight.Medium,
         )
 
         if (onClick != null) {
             Spacer(Modifier.weight(1f))
-            // TODO: Use MaterialTheme.typography style
-            Text(
-                text = stringResource(R.string.text_favorites_btn),
-                fontSize = 18.sp,
-                fontWeight = FontWeight.Bold,
-                color = MaterialTheme.colorScheme.primary,
-            )
+            SmallButton(
+                onClick = onClick
+            ) {
+                Text(text = "See all")
+            }
         }
     }
-}
-
-object HeaderDefaults {
-    val ContentPadding = PaddingValues(horizontal = 16.dp, vertical = 14.dp)
-    val ContentPaddingExcludeBottom = ContentPadding
-        .only(WindowInsetsSides.Horizontal + WindowInsetsSides.Top)
 }
