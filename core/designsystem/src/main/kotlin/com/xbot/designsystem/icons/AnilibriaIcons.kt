e16a7a5e18070248fb2a674a290e6627e6a30f22
Javokhir Isaev <xephosbot@gmail.com>
2025-02-26 17:32:11 +0300

    feat: add more icons to AnilibriaIcons
    Adds new icons to the `AnilibriaIcons` object:
    - `Outlined.Star`
    - `Outlined.CheckList`
    - `Outlined.PlayList`
    - `Filled.Star`
    - `Filled.Share`

--- core/designsystem/src/main/kotlin/com/xbot/designsystem/icons/AnilibriaIcons.kt
+++ core/designsystem/src/main/kotlin/com/xbot/designsystem/icons/AnilibriaIcons.kt
@@ -2,10 +2,14 @@ package com.xbot.designsystem.icons
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material.icons.automirrored.outlined.PlaylistPlay
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.Check
+import androidx.compose.material.icons.outlined.Checklist
 import androidx.compose.material.icons.outlined.ChevronLeft
 import androidx.compose.material.icons.outlined.ChevronRight
 import androidx.compose.material.icons.outlined.Clear
@@ -14,10 +18,12 @@ import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.PersonOutline
 import androidx.compose.material.icons.outlined.PlayArrow
 import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.outlined.Star
 
 object AnilibriaIcons {
     object Outlined {
         val Heart get() = Icons.Outlined.FavoriteBorder
+        val Star get() = Icons.Outlined.Star
         val Home get() = Icons.Outlined.Home
         val Person get() = Icons.Outlined.PersonOutline
         val Search get() = Icons.Outlined.Search
@@ -27,10 +33,14 @@ object AnilibriaIcons {
         val PlayArrow get() = Icons.Outlined.PlayArrow
         val Check get() = Icons.Outlined.Check
         val Clear get() = Icons.Outlined.Clear
+        val CheckList get() = Icons.Outlined.Checklist
+        val PlayList get() = Icons.AutoMirrored.Outlined.PlaylistPlay
     }
     object Filled {
         val Heart get() = Icons.Filled.Favorite
+        val Star get() = Icons.Filled.Star
         val Home get() = Icons.Filled.Home
         val Person get() = Icons.Filled.Person
+        val Share get() = Icons.Filled.Share
     }
 }
