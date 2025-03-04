79de0c46c48078db68112ceeaf8bbb7b10265615
Javokhir Isaev <xephosbot@gmail.com>
2025-03-04 23:20:07 +0300

    feat: Localization extensions added

--- core/common/src/main/java/com/xbot/common/localization/DayOfWeekExt.kt
+++ core/common/src/main/java/com/xbot/common/localization/DayOfWeekExt.kt
@@ -5,7 +5,5 @@ import java.time.format.TextStyle
 import java.util.Locale
 
 fun DayOfWeek.toLocalizedString(locale: Locale = Locale.getDefault()): String {
-    return getDisplayName(TextStyle.FULL, locale).run {
-        replaceFirstChar { it.uppercase() }
-    }
+    return getDisplayName(TextStyle.FULL, locale)
 }
