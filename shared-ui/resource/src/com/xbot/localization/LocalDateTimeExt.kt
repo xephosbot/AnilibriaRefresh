ad97f2715d2c5cf27b92950a757b560eb971440a
Javokhir Isaev <xephosbot@gmail.com>
2025-05-07 00:18:18 +0300

    Time format

--- shared-ui/resource/src/com/xbot/localization/LocalDateTimeExt.kt
+++ shared-ui/resource/src/com/xbot/localization/LocalDateTimeExt.kt
@@ -4,7 +4,7 @@ import androidx.compose.ui.text.intl.Locale
 import kotlinx.datetime.LocalDateTime
 
 fun LocalDateTime.toLocalizedString(locale: Locale = Locale.current): String {
-    return format(locale, "d MMM. yyyy HH:mm")
+    return format(locale, "d MMM yyyy \u2022 HH:mm")
 }
 
 expect fun LocalDateTime.format(locale: Locale, format: String): String
