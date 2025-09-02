d4bc2be41d027ac5e2c9ed9d5c00b71cd9ceec79
Javokhir Isaev <xephosbot@gmail.com>
2025-09-02 19:20:53 +0500

    time: Time library updated

--- shared-ui/resource/src/jvmMain/kotlin/com/xbot/localization/DayOfWeekExt.jvm.kt
+++ shared-ui/resource/src/jvmMain/kotlin/com/xbot/localization/DayOfWeekExt.jvm.kt
@@ -2,8 +2,9 @@ package com.xbot.localization
 
 import androidx.compose.ui.text.intl.Locale
 import kotlinx.datetime.DayOfWeek
+import kotlinx.datetime.toJavaDayOfWeek
 import java.time.format.TextStyle
 
 actual fun DayOfWeek.getName(locale: Locale): String {
-    return getDisplayName(TextStyle.FULL, locale.platformLocale)
+    return this.toJavaDayOfWeek().getDisplayName(TextStyle.FULL, locale.platformLocale)
 }
