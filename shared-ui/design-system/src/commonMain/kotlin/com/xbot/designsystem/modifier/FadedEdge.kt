bd6da7f21b1a2906669da3a34ed9de1eb1a3e3f2
Javokhir Isaev <xephosbot@gmail.com>
2025-09-04 22:22:58 +0500

    design-system: FadedEdge modifier updated

--- shared-ui/design-system/src/commonMain/kotlin/com/xbot/designsystem/modifier/FadedEdge.kt
+++ shared-ui/design-system/src/commonMain/kotlin/com/xbot/designsystem/modifier/FadedEdge.kt
@@ -2,7 +2,6 @@ package com.xbot.designsystem.modifier
 
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.CacheDrawScope
-import androidx.compose.ui.draw.DrawResult
 import androidx.compose.ui.draw.drawWithCache
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
@@ -18,39 +17,54 @@ fun Modifier.fadedEdge(
     edgeHeight: Dp = DefaultFadingEdgeHeight,
     opacity: Float = 1.0f,
     bottomEdge: Boolean = true,
-): Modifier = graphicsLayer {
-    compositingStrategy = CompositingStrategy.Offscreen
-}.drawWithCache {
-    val edgeHeightPx = edgeHeight.toPx()
-    drawFadedEdge(edgeHeightPx, opacity, bottomEdge)
-}
+): Modifier = fadedEdgeInternal(
+    opacity = opacity,
+    bottomEdge = bottomEdge
+) { size -> edgeHeight.toPx() }
 
 fun Modifier.fadedEdge(
     edgeHeightRatio: Float = 0.5f,
     opacity: Float = 1.0f,
     bottomEdge: Boolean = true
-): Modifier = graphicsLayer {
-     compositingStrategy = CompositingStrategy.Offscreen
-}.drawWithCache {
-    val edgeHeightPx = size.width * edgeHeightRatio
-    drawFadedEdge(edgeHeightPx, opacity, bottomEdge)
-}
+): Modifier = fadedEdgeInternal(
+    opacity = opacity,
+    bottomEdge = bottomEdge
+) { size -> size.height * edgeHeightRatio }
 
-private fun CacheDrawScope.drawFadedEdge(edgeHeight: Float, opacity: Float, bottomEdge: Boolean): DrawResult {
-    val brush = Brush.verticalGradient(
-        colors = listOf(Color.Transparent.copy(alpha = 1f - opacity), Color.Black),
-        startY = if (bottomEdge) size.height else 0f,
-        endY = if (bottomEdge) size.height - edgeHeight else edgeHeight,
-    )
-    return onDrawWithContent {
-        drawContent()
-        drawRect(
-            topLeft = Offset(0f, if (bottomEdge) size.height - edgeHeight else 0f),
-            size = Size(size.width, edgeHeight),
-            brush = brush,
-            blendMode = BlendMode.DstIn,
+
+private fun Modifier.fadedEdgeInternal(
+    opacity: Float,
+    bottomEdge: Boolean,
+    edgeHeightPx: CacheDrawScope.(Size) -> Float
+): Modifier = this.then(
+    graphicsLayer {
+        compositingStrategy = CompositingStrategy.Offscreen
+    }.drawWithCache {
+        val edgeHeight = edgeHeightPx(size).coerceAtLeast(0f)
+
+        val brush = Brush.verticalGradient(
+            colors = listOf(
+                Color.Transparent.copy(alpha = 1f - opacity),
+                Color.Black
+            ),
+            startY = if (bottomEdge) size.height else 0f,
+            endY = if (bottomEdge) size.height - edgeHeight else edgeHeight
         )
+
+        onDrawWithContent {
+            drawContent()
+            drawRect(
+                topLeft = Offset(
+                    x = 0f,
+                    y = if (bottomEdge) size.height - edgeHeight else 0f
+                ),
+                size = Size(width = size.width, height = edgeHeight),
+                brush = brush,
+                blendMode = BlendMode.DstIn
+            )
+        }
     }
-}
+)
+
 
 private val DefaultFadingEdgeHeight = 96.dp
