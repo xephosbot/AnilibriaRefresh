4f61aa684bbae5a10790de1cefd3d619ae1d9aef
Javokhir Isaev <xephosbot@gmail.com>
2025-09-06 01:26:06 +0500

    Add test data for Release model

---
+++ shared-ui/design-system/src/commonMain/kotlin/com/xbot/designsystem/utils/ReleaseTestData.kt
@@ -0,0 +1,89 @@
+package com.xbot.designsystem.utils
+
+import com.xbot.domain.models.Poster
+import com.xbot.domain.models.Release
+import com.xbot.domain.models.enums.AgeRating
+import com.xbot.domain.models.enums.ReleaseType
+
+internal val dummyReleaseList = listOf(
+    Release(
+        id = 10000,
+        type = ReleaseType.TV,
+        year = 2025,
+        name = "Дождь и ты",
+        englishName = "Ame to Kimi to",
+        description = "Одним дождливым днём, который ничем не отличался от предыдущих, Фудзи шла по улице и заметила на обочине коробку с запиской: \"Подберите меня, пожалуйста\". В самой же коробке сидело некое создание, похожее на собаку, которое, к большому удивлению Фудзи, протянуло ей зонтик. Поддавшись минутному порыву, девушка решила взять необычную собаку себе, и с того дня каждый новый день её жизни наполнен милотой и теплом.",
+        ageRating = AgeRating.R16_PLUS,
+        episodesCount = 12,
+        episodeDuration = 22,
+        favoritesCount = 3422,
+        poster = Poster(
+            src = "/storage/releases/posters/10000/bExfehD3Ohr2AOJAaoiwTyQGmn3P7atS.webp",
+            thumbnail = "/storage/releases/posters/10000/C7iM5uOHPf8TYUdrkcGFVXbJnPtMZFvE.webp",
+        )
+    ),
+    Release(
+        id = 9919,
+        type = ReleaseType.TV,
+        year = 2025,
+        name = "Карманы лета",
+        englishName = "Summer Pockets",
+        description = "Это экранизация визуальной новеллы от авторов \"Кланнад\" и \"Ангельские ритмы!\".\r\nНас ожидает увлекательная и трогательная история подростка, которому из-за семейных обстоятельств приходится поехать из оживлённого города на отдалённый островок, где он бывал в далёком детстве.\r\nТам он быстро заводит новых друзей, и, общаясь с ними, начинает забывать о городской суете и проникается неспешным ритмом жизни и красотой местной природы.\r\nВместе они исследуют остров, и эти небольшие приключения помогают ему наконец открыть для себя истинную прелесть беззаботных летних каникул.",
+        ageRating = AgeRating.R16_PLUS,
+        episodesCount = 26,
+        episodeDuration = 25,
+        favoritesCount = 5505,
+        poster = Poster(
+            src = "/storage/releases/posters/9919/dWQDZfDtO94PESwLPohrBsBRCtbjGFZh.webp",
+            thumbnail = "/storage/releases/posters/9919/C19u91HqPfXxpeeYAVH7csHYI6t1rDHW.webp",
+        )
+    ),
+    Release(
+        id = 10027,
+        type = ReleaseType.TV,
+        year = 2025,
+        name = "Сведённые кукушкой 2",
+        englishName = "Kakkou no Iinazuke Season 2",
+        description = "Ещё младенцем Наги Умино стал жертвой случая — его перепутали в роддоме с другим ребёнком. Спустя шестнадцать лет Нáги выпадает шанс увидеться со своими настоящими родителями, но и здесь судьба подкидывает ему сюрприз. По пути Нáги встречает Эрику Амано, которую по ошибке принимает за самоубийцу. Он спасает девушку, но на встрече с родителями выяснятся, что Наги перепутали в роддоме именно с Эрикой. Более того, родители хотят их теперь поженить! Наги таким поворотам не рад, ведь он влюблён в свою одноклассницу Хиро Сэгаву.\r\nЧереда событий вынуждает Эрику, Хиро и главного героя с его сводной сестрой Сати жить под одной крышей. Они и не подозревают, что в город так же возвращается давняя подруга детства Наги...",
+        ageRating = AgeRating.R16_PLUS,
+        episodesCount = 12,
+        episodeDuration = null,
+        favoritesCount = 3812,
+        poster = Poster(
+            src = "/storage/releases/posters/10027/99Sp9pUWNe0bsUCxvd02b5sZWQ3PZpBq.webp",
+            thumbnail = "/storage/releases/posters/10027/I7P1S860tyFEbRnBFzMgtGkFzxWyOfZR.webp"
+        )
+    ),
+    Release(
+        id = 9992,
+        type = ReleaseType.TV,
+        year = 2025,
+        name = "Клеватесс: Король демонических зверей, младенец и герой-нежить",
+        englishName = "Clevatess: Majuu no Ou to Akago to Shikabane no Yuusha",
+        description = "С самого детства Алисия мечтала стать отважной воительницей и защищать народ её королевства. И её мечта осуществилась – она оказалась одной из тринадцати героев, отобранных самим королём. Вооружив их легендарными мечами, монарх направил героев покорить своей власти короля демонических зверей Клеватесса. Однако благая миссия потерпела крах, унеся за собой жизни не только героев, но и жителей столицы королевства. Единственное, что смогло остановить уничтожение всего рода людского – это маленький ребёнок, о котором Клеватесса попросил позаботиться умирающий юноша.",
+        ageRating = AgeRating.R16_PLUS,
+        episodesCount = 12,
+        episodeDuration = null,
+        favoritesCount = 14776,
+        poster = Poster(
+            src = "/storage/releases/posters/9992/KCeDBgk6loihxysxp7R7zLat0s6J4qcS.webp",
+            thumbnail = "/storage/releases/posters/9992/82CQk0Jxga2tA41zxZ7mJNXGvqm9e6np.webp",
+        )
+    ),
+    Release(
+        id = 9984,
+        type = ReleaseType.TV,
+        year = 2025,
+        name = "Я переродился торговым автоматом и скитаюсь по лабиринту 2",
+        englishName = "Jidou Hanbaiki ni Umarekawatta Ore wa Meikyuu wo Samayou 2nd Season",
+        description = "Я переродился… Во что?! В торговый автомат?! Сенсационное аниме \"Я переродился торговым автоматом и скитаюсь по лабиринту\" расцветает новыми красками невероятных приключений!\r\nТорговый автомат Хаккон, дерзнувший бросить вызов самому владыке Подземного мира, воспрянет из пепла, чтобы плечом к плечу с лидерами Ассоциации охотников сойтись в решающей схватке. Но новая встреча с королем преступного мира обернется трагедией… Смогут ли Хаккон и его товарищи одолеть злодея, раскрыв тайны его былой одержимости торговыми автоматами, обретя верных союзников и преобразившись до неузнаваемости?",
+        ageRating = AgeRating.R16_PLUS,
+        episodesCount = null,
+        episodeDuration = null,
+        favoritesCount = 4519,
+        poster = Poster(
+            src = "/storage/releases/posters/9984/YkFoXhef1jtn7wJC4UCMMlJGVjBUkNmP.webp",
+            thumbnail = "/storage/releases/posters/9984/IyYz6EmEGxRR6uwnh6zesUD0rRfUwnQY.webp",
+        )
+    )
+)
