fc8bd4a8eab3e4ccfca9c6c4784c37306903de7c
Javokhir Isaev <xephosbot@gmail.com>
2025-09-08 18:59:50 +0500

    update getreleasesfeed to include scheduleNow bestReleases and recommendedFranchises for richer data fetching

--- shared/domain/src/commonMain/kotlin/com/xbot/domain/usecase/GetReleasesFeed.kt
+++ shared/domain/src/commonMain/kotlin/com/xbot/domain/usecase/GetReleasesFeed.kt
@@ -4,6 +4,10 @@ import arrow.core.Either
 import arrow.core.raise.either
 import com.xbot.domain.models.Error
 import com.xbot.domain.models.ReleasesFeed
+import com.xbot.domain.models.enums.SortingType
+import com.xbot.domain.models.filters.CatalogFilters
+import com.xbot.domain.repository.CatalogRepository
+import com.xbot.domain.repository.FranchisesRepository
 import com.xbot.domain.repository.GenresRepository
 import com.xbot.domain.repository.ReleasesRepository
 import com.xbot.domain.repository.ScheduleRepository
@@ -12,19 +16,37 @@ import kotlinx.coroutines.coroutineScope
 
 class GetReleasesFeed(
     private val releasesRepository: ReleasesRepository,
+    private val catalogRepository: CatalogRepository,
     private val scheduleRepository: ScheduleRepository,
+    private val franchisesRepository: FranchisesRepository,
     private val genresRepository: GenresRepository,
 ) {
     suspend operator fun invoke(): Either<Error, ReleasesFeed> = either {
         coroutineScope {
             val recommendedTitles = async { releasesRepository.getRandomReleases(10) }
-            val schedule = async { scheduleRepository.getScheduleWeek() }
+            val scheduleNow = async { scheduleRepository.getScheduleNow() }
+            val currentSeason = async { scheduleRepository.getCurrentSeason() }
+            val currentYear = async { scheduleRepository.getCurrentYear() }
+            val bestReleases = async {
+                catalogRepository.getCatalogReleases(
+                    search = null,
+                    filters = CatalogFilters(
+                        seasons = listOf(currentSeason.await().bind()),
+                        years = currentYear.await().bind().let { it..it },
+                        sortingTypes = listOf(SortingType.RATING_DESC)
+                    ),
+                    limit = 10
+                )
+            }
+            val recommendedFranchises = async { franchisesRepository.getRandomFranchises(10) }
             val genres = async { genresRepository.getRandomGenres(10) }
 
             ReleasesFeed(
                 recommendedReleases = recommendedTitles.await().bind(),
-                schedule = schedule.await().bind(),
-                genres = genres.await().bind()
+                scheduleNow = scheduleNow.await().bind(),
+                bestReleases = bestReleases.await().bind(),
+                recommendedFranchises = recommendedFranchises.await().bind(),
+                genres = genres.await().bind(),
             )
         }
     }
