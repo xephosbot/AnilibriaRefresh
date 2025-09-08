f4d512deb6a43203814313dac29d6efc1b567acb
Javokhir Isaev <xephosbot@gmail.com>
2025-09-08 18:57:39 +0500

    add default franchises repository implementation and update repository module for dependency injection

--- shared/data/src/commonMain/kotlin/com/xbot/data/di/RepositoryModule.kt
+++ shared/data/src/commonMain/kotlin/com/xbot/data/di/RepositoryModule.kt
@@ -2,12 +2,14 @@ package com.xbot.data.di
 
 import com.xbot.data.repository.DefaultAuthRepository
 import com.xbot.data.repository.DefaultCatalogRepository
+import com.xbot.data.repository.DefaultFranchisesRepository
 import com.xbot.data.repository.DefaultGenresRepository
 import com.xbot.data.repository.DefaultProfileRepository
 import com.xbot.data.repository.DefaultReleasesRepository
 import com.xbot.data.repository.DefaultScheduleRepository
 import com.xbot.domain.repository.AuthRepository
 import com.xbot.domain.repository.CatalogRepository
+import com.xbot.domain.repository.FranchisesRepository
 import com.xbot.domain.repository.GenresRepository
 import com.xbot.domain.repository.ProfileRepository
 import com.xbot.domain.repository.ReleasesRepository
@@ -19,6 +21,7 @@ import org.koin.dsl.module
 val repositoryModule = module {
     singleOf(::DefaultAuthRepository) { bind<AuthRepository>() }
     singleOf(::DefaultReleasesRepository) { bind<ReleasesRepository>() }
+    singleOf(::DefaultFranchisesRepository) { bind<FranchisesRepository>() }
     singleOf(::DefaultCatalogRepository) { bind<CatalogRepository>() }
     singleOf(::DefaultProfileRepository) { bind<ProfileRepository>() }
     singleOf(::DefaultScheduleRepository) { bind<ScheduleRepository>() }
