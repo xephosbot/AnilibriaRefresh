4ac118ea86aa4dd8bb8de1c1828c36aab06ffa1f
Javokhir Isaev <xephosbot@gmail.com>
2025-09-08 18:55:52 +0500

    update schedule api to return list of schedule dto for now and week endpoints

--- shared/network/src/commonMain/kotlin/com/xbot/network/api/ScheduleApi.kt
+++ shared/network/src/commonMain/kotlin/com/xbot/network/api/ScheduleApi.kt
@@ -3,20 +3,21 @@ package com.xbot.network.api
 import arrow.core.Either
 import com.xbot.network.client.NetworkError
 import com.xbot.network.client.request
+import com.xbot.network.models.dto.ScheduleDto
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 
 interface ScheduleApi {
-    suspend fun getScheduleNow(): Either<NetworkError, Map<String, com.xbot.network.models.dto.ScheduleDto>>
-    suspend fun getScheduleWeek(): Either<NetworkError, List<com.xbot.network.models.dto.ScheduleDto>>
+    suspend fun getScheduleNow(): Either<NetworkError, Map<String, List<ScheduleDto>>>
+    suspend fun getScheduleWeek(): Either<NetworkError, List<ScheduleDto>>
 }
 
 internal class DefaultScheduleApi(private val client: HttpClient) : ScheduleApi {
-    override suspend fun getScheduleNow(): Either<NetworkError, Map<String, com.xbot.network.models.dto.ScheduleDto>> = client.request {
+    override suspend fun getScheduleNow(): Either<NetworkError, Map<String, List<ScheduleDto>>> = client.request {
         get("anime/schedule/now")
     }
 
-    override suspend fun getScheduleWeek(): Either<NetworkError, List<com.xbot.network.models.dto.ScheduleDto>> = client.request {
+    override suspend fun getScheduleWeek(): Either<NetworkError, List<ScheduleDto>> = client.request {
         get("anime/schedule/week")
     }
 }
