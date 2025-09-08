43112f2696e3bc05855be9f77f39984d5f7c84c6
Javokhir Isaev <xephosbot@gmail.com>
2025-09-08 18:56:03 +0500

    refactor: update getFranchises to return list of franchises wrapped in either for better error handling and change getReleaseFranchises parameter to int for consistency

--- shared/domain/src/commonMain/kotlin/com/xbot/domain/repository/FranchisesRepository.kt
+++ shared/domain/src/commonMain/kotlin/com/xbot/domain/repository/FranchisesRepository.kt
@@ -1,12 +1,12 @@
 package com.xbot.domain.repository
 
-import androidx.paging.PagingSource
 import arrow.core.Either
+import com.xbot.domain.models.Error
 import com.xbot.domain.models.Franchise
 
 interface FranchisesRepository {
-    suspend fun getFranchises(): PagingSource<Int, Franchise>
+    suspend fun getFranchises(): Either<Error, List<Franchise>>
     suspend fun getFranchise(franchiseId: String): Either<Error, Franchise>
     suspend fun getRandomFranchises(limit: Int): Either<Error, List<Franchise>>
-    suspend fun getReleaseFranchises(releaseId: String): Either<Error, List<Franchise>>
+    suspend fun getReleaseFranchises(releaseId: Int): Either<Error, List<Franchise>>
 }
