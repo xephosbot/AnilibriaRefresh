6c2be9d9c0ed888e0036cb243533af2a2b495166
Javokhir Isaev <xephosbot@gmail.com>
2025-09-28 17:15:24 +0500

    fix: update data store creation to use temporary directory for file storage and simplify dependency injection setup

--- shared/data/src/jvmMain/kotlin/com/xbot/data/datasource/DataStoreFactory.jvm.kt
+++ shared/data/src/jvmMain/kotlin/com/xbot/data/datasource/DataStoreFactory.jvm.kt
@@ -1,2 +1,12 @@
 package com.xbot.data.datasource
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import java.io.File
+
+internal fun createDataStore(): DataStore<Preferences> = createDataStore(
+    producePath = {
+        val file = File(System.getProperty("java.io.tmpdir"), dataStoreFileName)
+        file.absolutePath
+    }
+)
--- shared/data/src/jvmMain/kotlin/com/xbot/data/di/DataStoreModule.jvm.kt
+++ shared/data/src/jvmMain/kotlin/com/xbot/data/di/DataStoreModule.jvm.kt
@@ -1,9 +1,8 @@
 package com.xbot.data.di
 
 import com.xbot.data.datasource.createDataStore
-import com.xbot.data.datasource.dataStoreFileName
 import org.koin.dsl.module
 
 actual val dataStoreModule = module {
-    single { createDataStore { dataStoreFileName } }
+    single { createDataStore() }
 }
