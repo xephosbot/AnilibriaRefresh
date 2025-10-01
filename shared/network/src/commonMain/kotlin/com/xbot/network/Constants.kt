4f4c2027001c5af9dde9460b4cc56125f8a78269
Javokhir Isaev <xephosbot@gmail.com>
2025-10-01 21:35:05 +0500

    fix: update base URLs to correct domain in network constants

--- shared/network/src/commonMain/kotlin/com/xbot/network/Constants.kt
+++ shared/network/src/commonMain/kotlin/com/xbot/network/Constants.kt
@@ -1,8 +1,8 @@
 package com.xbot.network
 
 object Constants {
-    internal const val BASE_URL: String = "https://anilibria.top"
-    internal const val BASE_URL_API: String = "https://anilibria.top/api/v1/"
+    internal const val BASE_URL: String = "https://aniliberty.top"
+    internal const val BASE_URL_API: String = "https://aniliberty.top/api/v1/"
 
     fun withBaseUrl(route: String) = "$BASE_URL$route"
 }
