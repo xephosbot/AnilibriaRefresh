cd55336b47ebec12f013f02ea1a0091429ace3cb
Javokhir Isaev <xephosbot@gmail.com>
2025-11-11 21:21:18 +0500

    refactor(domain): add UseCase suffix to use case classes
    The names of the use case classes have been updated to include the `UseCase` suffix for better clarity and consistency.
    
    Specifically, the following classes were renamed:
    - `GetReleasesFeed` -> `GetReleasesFeedUseCase`
    - `GetCatalogFilters` -> `GetCatalogFiltersUseCase`
    - `GetCatalogReleasesPager` -> `GetCatalogReleasesPagerUseCase`
    
    Additionally, `GetAuthStateUseCase` has been added to the domain DI module.

--- shared/domain/src/commonMain/kotlin/com/xbot/domain/di/DomainModule.kt
+++ shared/domain/src/commonMain/kotlin/com/xbot/domain/di/DomainModule.kt
@@ -1,15 +1,17 @@
 package com.xbot.domain.di
 
-import com.xbot.domain.usecase.GetCatalogFilters
-import com.xbot.domain.usecase.GetReleasesFeed
-import com.xbot.domain.usecase.GetCatalogReleasesPager
+import com.xbot.domain.usecase.GetAuthStateUseCase
+import com.xbot.domain.usecase.GetCatalogFiltersUseCase
+import com.xbot.domain.usecase.GetReleasesFeedUseCase
+import com.xbot.domain.usecase.GetCatalogReleasesPagerUseCase
 import com.xbot.domain.usecase.GetSortedScheduleWeekUseCase
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val domainModule = module {
-    singleOf(::GetReleasesFeed)
-    singleOf(::GetCatalogFilters)
-    singleOf(::GetCatalogReleasesPager)
+    singleOf(::GetAuthStateUseCase)
+    singleOf(::GetReleasesFeedUseCase)
+    singleOf(::GetCatalogFiltersUseCase)
+    singleOf(::GetCatalogReleasesPagerUseCase)
     singleOf(::GetSortedScheduleWeekUseCase)
 }
