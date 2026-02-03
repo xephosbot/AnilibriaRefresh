5ac8f425cd6b7ae17859229271940f74b491dc7e
Javokhir Isaev <xephosbot@gmail.com>
2026-02-03 22:24:32 +0500

    fix(ui): Remove unnecessary window insets

--- shared-ui/feature/preference/impl/src/commonMain/kotlin/com/xbot/preference/appearance/AppearancePane.kt
+++ shared-ui/feature/preference/impl/src/commonMain/kotlin/com/xbot/preference/appearance/AppearancePane.kt
@@ -2,12 +2,10 @@ package com.xbot.preference.appearance
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ButtonDefaults
@@ -109,7 +107,6 @@ private fun AppearanceScreenContent(
             )
         },
         containerColor = MaterialTheme.colorScheme.surfaceContainer,
-        contentWindowInsets = WindowInsets.safeDrawing,
     ) { innerPadding ->
         Column(
             modifier = Modifier
