a7bdfb23932eda7f359e44817d87460d9bd997a2
Javokhir Isaev <xephosbot@gmail.com>
2026-04-26 17:29:38 +0500

    refactor(network): remove connectivity polling logs in JVM implementation

--- shared/core/network/impl/src/jvmMain/kotlin/com/xbot/network/client/ConnectivityMonitor.jvm.kt
+++ shared/core/network/impl/src/jvmMain/kotlin/com/xbot/network/client/ConnectivityMonitor.jvm.kt
@@ -1,8 +1,6 @@
 package com.xbot.network.client
 
-import co.touchlab.kermit.Logger
 import dev.jordond.connectivity.Connectivity
-import dev.jordond.connectivity.PollResult
 import org.koin.core.annotation.Singleton
 
 @Singleton
@@ -11,11 +9,4 @@ internal fun createConnectivityMonitor(): Connectivity = Connectivity {
     urls("https://www.cloudflare.com/")
     pollingIntervalMs = 5.minutes
     timeoutMs = 2.seconds
-
-    onPollResult { result ->
-        when (result) {
-            is PollResult.Error -> Logger.e("Poll error", result.throwable)
-            is PollResult.Response -> Logger.d("Poll http response: {${result.response}")
-        }
-    }
 }
