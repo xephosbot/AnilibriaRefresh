aed7713b64f0d13b0ef84c7a11eb551c4df1ba36
Javokhir Isaev <xephosbot@gmail.com>
2026-05-01 20:14:43 +0500

    build: refactor Kotlin configuration logic

--- build-logic/convention/src/main/kotlin/KotlinMultiplatformConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/KotlinMultiplatformConventionPlugin.kt
@@ -1,17 +1,14 @@
 import com.android.build.api.AndroidPluginVersion
 import com.android.build.api.dsl.CommonExtension
-import com.android.build.api.dsl.KotlinMultiplatformAndroidLibraryExtension
 import com.android.build.api.dsl.KotlinMultiplatformAndroidLibraryTarget
-import com.xbot.convention.applyPlugin
+import com.xbot.convention.configureKotlin
 import com.xbot.convention.configureKotlinAndroid
 import com.xbot.convention.configureKotlinJvm
 import com.xbot.convention.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.plugins.JavaPluginExtension
-import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.getByType
-import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinAndroidTarget
@@ -19,38 +16,45 @@ import org.jetbrains.kotlin.gradle.targets.jvm.KotlinJvmTarget
 
 class KotlinMultiplatformConventionPlugin : Plugin<Project> {
     override fun apply(project: Project) = with(project) {
-        applyPlugin(libs.plugins.kotlin.multiplatform)
-
-        val kmpExtension = extensions.getByType<KotlinMultiplatformExtension>()
-        kmpExtension.targets.configureEach {
-            when (platformType) {
-                KotlinPlatformType.jvm -> {
-                    val target = (this as KotlinJvmTarget)
-                    println("JVM ${this::class.simpleName}")
-                    //val temp = listOf(KotlinJvmTarget(), )
-                    //configureKotlinJvm(this as KotlinJvmTarget)
-                }
-
-                KotlinPlatformType.androidJvm -> {
-                    if (this is KotlinAndroidTarget) {
-                        // Old kmp android integration using com.android.library or com.android.application
-                        val androidExtension = extensions.getByType<CommonExtension>()
-                        configureKotlinAndroid(androidExtension)
+        pluginManager.withPlugin(libs.plugins.kotlin.multiplatform.get().pluginId) {
+            val kmpExtension = extensions.getByType<KotlinMultiplatformExtension>()
+            kmpExtension.targets.configureEach {
+                when (platformType) {
+                    KotlinPlatformType.jvm -> {
+                        val target1 = (this as KotlinJvmTarget)
+                        val target2 = (this as KotlinJvmProjectExtension)
+                        val target3 = target2.target
+                        println("JVM ${this::class.simpleName}")
+                        val temp = listOf(target1, target2)
+                        //configureKotlinJvm(this as KotlinJvmTarget)
                     }
-                    if (AndroidPluginVersion.getCurrent() >= AndroidPluginVersion(8, 8) &&
-                        this is KotlinMultiplatformAndroidLibraryTarget
-                    ) {
-                        // New kmp android integration using com.android.multiplatform.library
-                        configureKotlinAndroid(this)
+
+                    KotlinPlatformType.androidJvm -> {
+                        if (this is KotlinAndroidTarget) {
+                            // Old kmp android integration using com.android.library or com.android.application
+                            val androidExtension = extensions.getByType<CommonExtension>()
+                            configureKotlinAndroid(androidExtension)
+                        }
+                        if (AndroidPluginVersion.getCurrent() >= AndroidPluginVersion(8, 8) &&
+                            this is KotlinMultiplatformAndroidLibraryTarget
+                        ) {
+                            // New kmp android integration using com.android.multiplatform.library
+                            configureKotlinAndroid(this)
+                        }
                     }
-                }
 
-                KotlinPlatformType.native -> {
-                    println("NATIVE ${this::class.simpleName}")
-                }
+                    KotlinPlatformType.native -> {
+                        println("NATIVE ${this::class.simpleName}")
+                    }
 
-                else -> { /*Nothing to do*/ }
+                    else -> { /*Nothing to do*/ }
+                }
             }
+            configureKotlin<KotlinMultiplatformExtension>()
+        }
+
+        pluginManager.withPlugin(libs.plugins.kotlin.jvm.get().pluginId) {
+            configureKotlinJvm()
         }
 
         extensions.extensionsSchema.elements.forEach { ext ->
--- build-logic/convention/src/main/kotlin/com/xbot/convention/ConfigureKotlin.kt
+++ build-logic/convention/src/main/kotlin/com/xbot/convention/ConfigureKotlin.kt
@@ -2,6 +2,7 @@ package com.xbot.convention
 
 import com.android.build.api.dsl.CommonExtension
 import com.android.build.api.dsl.KotlinMultiplatformAndroidLibraryExtension
+import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.kotlin.dsl.assign
@@ -20,22 +21,30 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 internal fun Project.configureKotlinAndroid(
     commonExtension: CommonExtension,
 ) {
-    configureKotlin<KotlinAndroidProjectExtension>()
+    with(commonExtension) {
+        this
+    }
 }
 
 internal fun Project.configureKotlinAndroid(
     multiplatformExtension: KotlinMultiplatformAndroidLibraryExtension,
 ) {
-    configureKotlin<KotlinMultiplatformExtension>()
+    with(multiplatformExtension) {
+        this.compileSdk {
+            this.version
+        }
+    }
 }
 
-internal fun Project.configureKotlinJvm(
-    jvmExtension: JavaPluginExtension
-) {
+internal fun Project.configureKotlinJvm() {
+    extensions.configure<JavaPluginExtension> {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
     configureKotlin<KotlinJvmProjectExtension>()
 }
 
-private inline fun <reified T : KotlinBaseExtension> Project.configureKotlin() = configure<T> {
+internal inline fun <reified T : KotlinBaseExtension> Project.configureKotlin() = configure<T> {
     val warningsAsErrors = providers.gradleProperty("warningsAsErrors").map {
         it.toBoolean()
     }.orElse(false)
